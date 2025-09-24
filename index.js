0838a2be89b3cd5dc2ffaca94db7511e73b431f8
lagerdigal1996 <kpogohhymes@hotmail.com>
2025-09-24 09:03:21 -0500

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import './styles.css';
+
+const app = document.getElementById('app');
+
+app.innerHTML = '<h1>Welcome to Wireframe Toolbox</h1><p>Your design journey starts here!</p>';
+console.log('Wireframe Toolbox Loaded');
